f9ddf23ce1194c4be73a9fd3fcf6576af17443f9
katreddyvenkateswaraReddy <63310358+katreddyvenkateswaraReddy@users.noreply.github.com>
2020-04-11 12:17:43 +0530

    Update scheduling program .c

--- scheduling program .c
+++ scheduling program .c
@@ -2,22 +2,24 @@
 #include<stdio.h> 
 int main() 
 { 
-  int count,j,n,time,remain,flag=0,time_quantum; 
-  int wait_time=0,turnaround_time=0,at[10],bt[10],rt[10]; 
-  printf("Enter Total Process:\t "); 
+  int count,j,n;
+  int time,remaining;
+  int flag=0,time_quantum=10; 
+  int waiting_time=0,turn_around_time=0,arrival_time[10],burst_time[10],rt[10]; 
+  printf("\n\nEnter the Total number of Process:\t "); 
   scanf("%d",&n); 
-  remain=n; 
+  remaining=n; 
   for(count=0;count<n;count++) 
   { 
-    printf("Enter Arrival Time and Burst Time for  Process Number %d :",count+1); 
-    scanf("%d",&at[count]); 
-    scanf("%d",&bt[count]); 
-    rt[count]=bt[count]; 
+    printf("Enter Arrival Time and Burst Time for Process Process Number %d :",count+1); 
+    scanf("%d",&arrival_time[count]); 
+    scanf("%d",&burst_time[count]); 
+    rt[count]=burst_time[count]; 
   } 
-  printf("Enter Time Quantum:\t"); 
-  scanf("%d",&time_quantum); 
+  printf("Enter Time Quantum:%d\t",time_quantum); 
+
   printf("\n\nProcess\t|Turnaround Time|Waiting Time\n\n"); 
-  for(time=0,count=0;remain!=0;) 
+  for(time=0,count=0;remaining!=0;) 
   { 
     if(rt[count]<=time_quantum && rt[count]>0) 
     { 
@@ -32,21 +34,21 @@ int main()
     } 
     if(rt[count]==0 && flag==1) 
     { 
-      remain--; 
-      printf("P[%d]\t|\t%d\t|\t%d\n",count+1,time-at[count],time-at[count]-bt[count]); 
-      wait_time+=time-at[count]-bt[count]; 
-      turnaround_time+=time-at[count]; 
+      remaining--; 
+      printf("P[%d]\t|\t%d\t|\t%d\n",count+1,time-arrival_time[count],time-arrival_time[count]-burst_time[count]); 
+      waiting_time+=time-arrival_time[count]-burst_time[count]; 
+      turn_around_time+=time-arrival_time[count]; 
       flag=0; 
     } 
     if(count==n-1) 
       count=0; 
-    else if(at[count+1]<=time) 
+    else if(arrival_time[count+1]<=time) 
       count++; 
     else 
       count=0; 
   } 
-  printf("\nAverage Waiting Time= %f\n",wait_time*1.0/n); 
-  printf("Avg Turnaround Time = %f",turnaround_time*1.0/n); 
-  
+  printf("\nAverage Waiting Time= %f\n",waiting_time*1.0/n); 
+  printf("Avg Turnaround Time = %f",turn_around_time*1.0/n); 
+
   return 0; 
 }
